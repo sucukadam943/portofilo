01210a0493d598045f30dff5ca2e0a377348368f
sucukadam943 <129880014+sucukadam943@users.noreply.github.com>
2024-06-01 13:15:45 +0300

    Add files via upload

---
+++ portfolio_tur-main/portfolio_tur-main/main.py
@@ -0,0 +1,27 @@
+# Import
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+# İçerik sayfasını çalıştırma
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Dinamik beceriler
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    return render_template('index.html', button_python=button_python)
+
+@app.route('/', methods=['POST'])
+def process_form():
+    button_discord = request.form.get('button_discord')
+    return render_template('index.html', button_discord=button_discord)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
